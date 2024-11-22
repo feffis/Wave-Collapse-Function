55dc4346f3be2c652380cdd7c99a61bac6bcad5c
stefania sanchez <stefani2019sanchez@gmail.com>
2024-11-22 05:59:56 -0500

    uno mas

--- sketch.js
+++ sketch.js
@@ -114,23 +114,89 @@ function setup() {
 }
 
 function draw() {
-    const celdaDis = celdas.filter((celda) => {
-        return celda.colapsada == false;
-      });
-    
-      if (celdaDis.length > 0) {
-        celdaDis.sort((a, b) => {
-          return a.opciones.length - b.opciones.length;
-        });
-        const celdasXColapsar = celdaDis.filter((celda) => {
-          return celda.opciones.length == celdaDis[0].opciones.length;
-        });
-    
-        const celdaSelec = random(celdasXColapsar);
-        celdaSelec.colapsada = true;
-    
-        const opcionSelec = random(celdaSelec.opciones);
-        celdaSelec.opciones = [opcionSelec];
-    
+  const celdaDis = celdas.filter((celda) => {
+    return celda.colapsada == false;
+  });
 
+  if (celdaDis.length > 0) {
+    celdaDis.sort((a, b) => {
+      return a.opciones.length - b.opciones.length;
+    });
+    const celdasXColapsar = celdaDis.filter((celda) => {
+      return celda.opciones.length == celdaDis[0].opciones.length;
+    });
+
+    const celdaSelec = random(celdasXColapsar);
+    celdaSelec.colapsada = true;
+
+    const opcionSelec = random(celdaSelec.opciones);
+    celdaSelec.opciones = [opcionSelec];
+
+    print(opcionSelec);
+
+    for (let x = 0; x < RETICULA; x++) {
+      for (let y = 0; y < RETICULA; y++) {
+        const celdaIndex = x + y * RETICULA;
+        const celdaActual = celdas[celdaIndex];
+        if (celdaActual.colapsada) {
+          const indiceAzulejo = celdaActual.opciones[0];
+          image(azulejos[indiceAzulejo], x * ancho, y * alto, ancho, alto);
+        } else {
+          strokeWeight(4);
+          rect(x * ancho, y * alto, ancho, alto);
+        }
+      }
+    }
+
+    // Actualizar las opciones de las celdas vecinas
+    for (let x = 0; x < RETICULA; x++) {
+      for (let y = 0; y < RETICULA; y++) {
+        const celdaIndex = x + y * RETICULA;
+        const celdaActual = celdas[celdaIndex];
+
+        if (celdaActual.colapsada) {
+          const indiceAzulejo = celdaActual.opciones[0];
+          const reglasActuales = reglas[indiceAzulejo];
+
+          // Monitorear UP
+          if (y > 0) {
+            const indiceUP = x + (y - 1) * RETICULA;
+            const celdaUp = celdas[indiceUP];
+            if (!celdaUp.colapsada) {
+              cambiarDireccion(celdaUp, reglasActuales["UP"], "DOWN");
+            }
+          }
+
+          // Monitorear RIGHT
+          if (x < RETICULA - 1) {
+            const indiceRight = x + 1 + y * RETICULA;
+            const celdaRight = celdas[indiceRight];
+            if (!celdaRight.colapsada) {
+              cambiarDireccion(celdaRight, reglasActuales["RIGHT"], "LEFT");
+            }
+          }
+
+          // Monitorear DOWN
+          if (y < RETICULA - 1) {
+            const indiceDown = x + (y + 1) * RETICULA;
+            const celdaDown = celdas[indiceDown];
+            if (!celdaDown.colapsada) {
+              cambiarDireccion(celdaDown, reglasActuales["DOWN"], "UP");
+            }
+          }
+
+          // Monitorear LEFT
+          if (x > 0) {
+            const indiceLeft = x - 1 + y * RETICULA;
+            const celdaLeft = celdas[indiceLeft];
+            if (!celdaLeft.colapsada) {
+              cambiarDireccion(celdaLeft, reglasActuales["LEFT"], "RIGHT");
+            }
+          }
+        }
+      }
+    }
+  } else {
+    noLoop();
+  }
 }
